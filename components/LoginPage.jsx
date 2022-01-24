cd9baeed0cf585aa1f6c68039c7002b8872dd2bb
Eden O'Brien <eobr@Edens-MacBook-Air.local>
2022-01-24 12:15:09 +0000

    created components folder. created LoginPage component

---
+++ components/LoginPage.jsx
@@ -0,0 +1,22 @@
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { TextInput } from "react-native-gesture-handler";
+
+const [usernameInput, setUsernameInput] = useState("");
+
+const LoginPage = () => {
+  return (
+    <View>
+        <Text>Username: </Text>
+      <TextInput >
+
+      </TextInput>
+      
+    </View>
+  );
+};
+
+export default LoginPage;
+
+
+
