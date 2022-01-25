c1d1f32660bfa62a4779b5323f81138c4b84e98a
Adam <adamrleigh96@gmail.com>
2022-01-25 16:16:02 +0000

    add method to get items

--- screens/ToolshedScreen.jsx
+++ screens/ToolshedScreen.jsx
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { db, storage } from "../firebase";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
 
 const ToolshedScreen = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const itemList = await getDocs(collection(db, "items"));
+        const itemArray = [];
+        itemList.forEach((doc) => itemArray.push(doc.data()));
+        setItems(itemArray);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  console.log(items);
+
   return (
     <View style={styles.container}>
       <Text>Toolshed</Text>
+      {items.map((item) => {
+        return <Text>{`\n${item.name}`}</Text>;
+      })}
     </View>
   );
 };
