dd7b5a0709c380c993b6fe54cce91a9fc3a0c197
magnit <90092645+hevmag@users.noreply.github.com>
2022-01-25 13:30:18 +0000

    renamed file and added screen navigation

---
+++ components/NavTabs.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+// import HomeScreen from "../screens/HomeScreen";
+import UserScreen from "../screens/UserScreen";
+import RequestsScreen from "../screens/RequestsScreen";
+import InboxScreen from "../screens/InboxScreen";
+import ToolshedScreen from "../screens/ToolshedScreen";
+
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const Tab = createMaterialBottomTabNavigator();
+
+function NavTabs() {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen
+        name="Toolshed"
+        component={ToolshedScreen}
+        options={{
+          tabBarLabel: "Toolshed",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          tabBarLabel: "My Page",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Requests"
+        component={RequestsScreen}
+        options={{
+          tabBarLabel: "Requests",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="help" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+
+export default NavTabs;
