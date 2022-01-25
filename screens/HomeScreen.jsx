2d0c6732d4d90ae1498cf34101eea5f244cd02db
Adam <adamrleigh96@gmail.com>
2022-01-25 10:39:42 +0000

    Added Homescreen to screens

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { TextInput } from "react-native-gesture-handler";
+
+const HomeScreen = () => {
+
+    
+
+  return (
+    <View>
+      <TextInput value={} />
+    </View>
+  );
+};
+
+export default HomeScreen;
