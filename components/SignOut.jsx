2ee90bcc2c8db75ef4772032d33c9279f9f5a139
k-huggs <kyle_huggins@hotmail.co.uk>
2022-01-26 10:22:34 +0000

    User documents added to users collection on sign up, sign out button component created

---
+++ components/SignOut.jsx
@@ -0,0 +1,43 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import { auth } from "../firebase";
+
+const SignOut = () => {
+  const handleSignOut = async () => {
+    try {
+      const signOut = await auth.signOut();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+        <Text style={styles.text}>Sign Out</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#0782f9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
+
+export default SignOut;
--- screens/Register.jsx
+++ screens/Register.jsx
@@ -1,50 +1,145 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from "react-native";
 import { createUserWithEmailAndPassword } from "@firebase/auth";
-import auth from "../firebase";
+import { auth, db } from "../firebase";
+import { setDoc, doc } from "firebase/firestore";
 
-const Register = ({navigation}) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const Register = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [surname, setSurname] = useState("");
 
-    const handleSignUp = async () => {
-        if (email !== "" && password !== "") {
-            try {
-                const newUser = await createUserWithEmailAndPassword(auth, email, password);
-            }
-            catch (err) {
-                alert("Signup error: Please choose a password that is 6 characters long");
-            }
-        }
+  const handleSignUp = async () => {
+    if (email !== "" && password !== "") {
+      try {
+        const newUser = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const docRef = newUser.user.uid;
+        let newUserInfo = { firstName, surname, email };
+        setDoc(doc(db, "users", docRef), newUserInfo);
+        alert("Sign Up Was Successful");
+        navigation.navigate("LoginScreen");
+      } catch (err) {
+        console.log(err);
+      }
     }
-    return (
-        <KeyboardAvoidingView behavior="padding">
-            <Text>Create a new account</Text>
-            <View>
-                <TextInput placeholder="Enter email"
-                autoCapitalize="none"
-                keyboardType="email-address"
-                textContentType="emailAddress"
-                value={email}
-                onChangeText={(text) => setEmail(text)}/>
-                <TextInput placeholder="Enter password"
-                autoCapitalize="none"
-                autoCorrect={false}
-                secureTextEntry={true}
-                textContentType="password"
-                value={password}
-                onChangeText={(text) => setPassword(text)}/>
-            </View>
-            <TouchableOpacity onPress={handleSignUp}>
-          <Text>Sign Up</Text>
+  };
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <Text style={styles.create}>Create a new account</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter First Name"
+          autoCapitalize="none"
+          textContentType="name"
+          value={firstName}
+          onChangeText={(text) => setFirstName(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Surname"
+          autoCapitalize="none"
+          textContentType="name"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+          textContentType="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+          <Text style={styles.buttonText}>Sign Up</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={() => navigation.navigate("Login")}
+          style={styles.button}
+          onPress={() => navigation.navigate("LoginScreen")}
         >
-          <Text>Go To Login</Text>
+          <Text style={styles.buttonText}>Go To Login</Text>
         </TouchableOpacity>
-        </KeyboardAvoidingView>
-    )
-}
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  create: {
+    marginBottom: 40,
+    fontWeight: "800",
+    fontSize: 20,
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#0782f9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782f9",
+    borderWidth: 2,
+  },
+  buttonOutlineText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
 
 export default Register;
