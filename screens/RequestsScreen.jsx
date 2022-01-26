6037d0f8d6bff48f674c4e5786497aa70b92ddfd
magnit <90092645+hevmag@users.noreply.github.com>
2022-01-26 20:36:59 +0000

    added postitem button and navigation to requestsscreen

--- screens/RequestsScreen.jsx
+++ screens/RequestsScreen.jsx
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 import React from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const RequestsScreen = () => {
+const RequestsScreen = ({ navigation }) => {
+
+  const handlePress = () => {
+    navigation.navigate("PostItem")
+  }
     return (
         <View style={styles.container}>
-      <Text>Requests screen/Toolboard</Text>
+        <Text>Requests screen/Toolboard</Text>
+        <Pressable style={styles.button} onPress={handlePress}>
+          <Text style={styles.text}>Post a tool!</Text>
+        </Pressable>
     </View>
   );
 };
@@ -12,9 +20,19 @@ const RequestsScreen = () => {
 export default RequestsScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems:  "center",
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#F36433",
+    margin: "5%",
+    padding: 10,
+    borderRadius: 5,
+  },
+
+  text: {
+    color: "#FFF8F0",
+  },
 });
