457f4ab957f1ad3c03ba530425196a00fe8b22ea
Adam <adamrleigh96@gmail.com>
2022-01-28 10:23:15 +0000

    created utils and added distance calculator function

---
+++ utils.js
@@ -0,0 +1,30 @@
+import { storage } from "./firebase";
+import { ref, getDownloadURL } from "firebase/storage";
+import { Image } from "react-native";
+import React, { useState, useEffect } from "react";
+
+export const distance = (loc1, loc2) => {
+  //Distance in km
+  const [
+    { latitude: lat1, longitude: lon1 },
+    { latitude: lat2, longitude: lon2 },
+  ] = [loc1, loc2];
+  if (lat1 == lat2 && lon1 == lon2) {
+    return 0;
+  } else {
+    var [radlat1, radlat2] = [(Math.PI * lat1) / 180, (Math.PI * lat2) / 180];
+    var theta = lon1 - lon2;
+    var radtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    dist = dist * 1.609344;
+    return dist.toFixed(2);
+  }
+};
