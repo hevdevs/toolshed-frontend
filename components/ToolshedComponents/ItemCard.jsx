c795e4538f7c8c835ac1d39e0b884d100747b518
magnit <90092645+hevmag@users.noreply.github.com>
2022-01-29 14:17:51 +0000

    added lent status and toggle switch to change items availibility

--- components/ToolshedComponents/ItemCard.jsx
+++ components/ToolshedComponents/ItemCard.jsx
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Switch } from "react-native";
 import React, { useEffect, useState } from "react";
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
+import { auth } from "../../firebase";
+
+import { Ionicons } from "@expo/vector-icons";
 
 const ItemCard = ({ item }) => {
   const storage = getStorage();
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [isLent, setIsLent] = useState(false);
   const [itemImage, setItemImage] = useState("");
   
   useEffect(() => {
@@ -19,6 +24,13 @@ const ItemCard = ({ item }) => {
     })();
   }, []);
 
+  const toggleAvailibility = () => {
+    setIsEnabled(previousState => !previousState)
+    setIsLent((previousState) => !previousState);
+    item.available = !isLent
+    console.log(item.available)
+  }
+
   return (
     <View style={styles.card}>
       {itemImage ? (
@@ -26,7 +38,27 @@ const ItemCard = ({ item }) => {
       ) : null}
       <View>
         <Text style={styles.cardText}>{`${item.name} \n`}</Text>
-        <Text style={styles.cardText}>{`${item.owner}`}</Text>
+        <Text style={styles.cardText}>
+          Posted by {item.userInfo.userUsername}
+        </Text>
+        {isLent === true ? (
+          <View style={styles.statusLent}>
+            <Text style={styles.text}>
+              LENT <Ionicons name={"close-circle"} size={16} />
+            </Text>
+          </View>
+        ) : null}
+        {item.userInfo.userUsername === auth.currentUser.displayName ? (
+          <View>
+            <Switch
+              style={styles.switch}
+              trackColor={{ false: "#9DD9D2", true: "#F36433" }}
+              thumbColor={isEnabled ? "#2DC2BD" : "#2DC2BD"}
+              onValueChange={toggleAvailibility}
+              value={isEnabled}
+            />
+          </View>
+        ) : null}
       </View>
     </View>
   );
@@ -37,10 +69,11 @@ export default ItemCard;
 const styles = StyleSheet.create({
   card: {
     flexDirection: "row",
+    alignItems: "center",
     width: "100%",
     padding: "5%",
     borderRadius: 5,
-    marginBottom: "5%",
+    margin: "5%",
     backgroundColor: "#FFF8F0",
   },
   cardText: {
@@ -52,5 +85,24 @@ const styles = StyleSheet.create({
     height: 100,
     padding: 0,
     margin: 0,
+  },
+  statusLent: {
+    backgroundColor: "#F36433",
+    padding: 10,
+    borderRadius: 5,
+    justifyContent: "center",
+    bottom: -25,
+    left: -120,
+    position: "absolute",
+  },
+  switch: {
+    bottom: -35,
+    right: -70,
+    position: "absolute",
+  },
+  text: {
+    color: "#FFF8F0",
+    fontWeight: "bold",
+    alignSelf: "center",
   },
 });
