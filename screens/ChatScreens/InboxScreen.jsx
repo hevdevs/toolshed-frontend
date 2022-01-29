f457321b326a3e0bb406e997fac15a6e9243344e
Adam <adamrleigh96@gmail.com>
2022-01-29 14:13:38 +0000

    start adding chats to inbox page

--- screens/ChatScreens/InboxScreen.jsx
+++ screens/ChatScreens/InboxScreen.jsx
@@ -1,10 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { getUserDataFromUid } from "../../utils";
+import { auth } from "../../firebase";
+import ChatCard from "../../components/ChatCard";
 const InboxScreen = () => {
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const currentUserInfo = await getUserDataFromUid(auth.currentUser.uid);
+        setChats(currentUserInfo.chats);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  console.log(chats);
+
   return (
     <View style={styles.container}>
-      <Text>Inbox Screen</Text>
+      <Text>chats: </Text>
+      {chats.map((chat) => (
+        <ChatCard chat={chat} />
+      ))}
     </View>
   );
 };
