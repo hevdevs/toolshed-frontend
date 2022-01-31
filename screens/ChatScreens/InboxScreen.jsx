9cb4702b8dc458e3540c965e2b35d45c9a74a68f
Adam <adamrleigh96@gmail.com>
2022-01-31 12:27:39 +0000

    setup real time inbox listening

--- screens/ChatScreens/InboxScreen.jsx
+++ screens/ChatScreens/InboxScreen.jsx
@@ -1,34 +1,48 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useLayoutEffect } from "react";
 import { getUserDataFromUid } from "../../utils";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
 import ChatCard from "../../components/ChatCard";
-const InboxScreen = () => {
+import { onSnapshot, doc } from "firebase/firestore";
+const InboxScreen = ({ navigation }) => {
   const [chats, setChats] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const currentUserInfo = await getUserDataFromUid(auth.currentUser.uid);
-        setChats(currentUserInfo.chats);
-      } catch (err) {
-        console.log(err);
+  useLayoutEffect(() => {
+    const unsubscribe = onSnapshot(
+      doc(db, "users", auth.currentUser.uid),
+      (userData) => {
+        const { chats } = userData.data();
+        setChats(chats);
       }
-    })();
+    );
+    return unsubscribe;
   }, []);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const currentUserInfo = await getUserDataFromUid(auth.currentUser.uid);
+  //       setChats(currentUserInfo.chats);
+  //     } catch (err) {
+  //       console.log("HERE");
+
+  //       console.log(err);
+  //     }
+  //   })();
+  // }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Your Inbox Page</Text>
       <View style={styles.contentContainer}>
-        {!chats ? (
+        {!chats.length ? (
           <View style={styles.title}>
             <Text style={styles.noMessage}>No Current Direct Messages</Text>
           </View>
         ) : (
           chats.map((chat, index) => (
             <View style={styles.chatCard} key={index}>
-              <ChatCard chat={chat} />
+              <ChatCard chat={chat} navigation={navigation} />
             </View>
           ))
         )}
@@ -36,9 +50,6 @@ const InboxScreen = () => {
     </View>
   );
 };
-
-export default InboxScreen;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -67,4 +78,12 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 20,
   },
+  title: {
+    fontWeight: "bold",
+  },
+  chatCard: {
+    fontWeight: "bold",
+  },
 });
+
+export default InboxScreen;
