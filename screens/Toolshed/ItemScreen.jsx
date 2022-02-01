a362893645d8cebc61be6ea3aaf08718413b5864
magnit <90092645+hevmag@users.noreply.github.com>
2022-02-01 15:25:35 +0000

    fixed img position

--- screens/Toolshed/ItemScreen.jsx
+++ screens/Toolshed/ItemScreen.jsx
@@ -1,14 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  SafeAreaView,
-  Image,
-  Pressable,
-} from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 
 import { getDownloadURL, ref } from "@firebase/storage";
 import { storage, auth, db } from "../../firebase";
@@ -71,7 +63,7 @@ const ItemScreen = ({ route, navigation }) => {
         </View>
         {/* <CalendarComponent /> */}
 
-        <TouchableOpacity
+        <Pressable
           style={styles.button}
           onPress={handlePress}
           itemOwner={item.owner}
@@ -79,7 +71,7 @@ const ItemScreen = ({ route, navigation }) => {
           <Text style={styles.text}>
             MESSAGE <Ionicons name={"paper-plane"} size={16} />
           </Text>
-        </TouchableOpacity>
+        </Pressable>
         <Pressable
           title="View Map"
           style={styles.button}
