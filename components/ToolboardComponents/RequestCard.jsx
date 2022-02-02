472e13bf6cf772dc2bd05bb123d5df86fe67f26f
Adam <adamrleigh96@gmail.com>
2022-02-02 11:58:18 +0000

    remove no current requests message

--- components/ToolboardComponents/RequestCard.jsx
+++ components/ToolboardComponents/RequestCard.jsx
@@ -4,41 +4,51 @@ import React from "react";
 import uuid from "react-native-uuid";
 
 import AppLoading from "expo-app-loading";
-import { Oxygen_400Regular, Oxygen_700Bold, useFonts } from "@expo-google-fonts/oxygen";
+import {
+  Oxygen_400Regular,
+  Oxygen_700Bold,
+  useFonts,
+} from "@expo-google-fonts/oxygen";
 
 const RequestCard = ({ requests, navigation }) => {
-
   let [fontsLoaded] = useFonts({
-    Oxygen_400Regular, Oxygen_700Bold,
+    Oxygen_400Regular,
+    Oxygen_700Bold,
   });
-  
+
   if (!fontsLoaded) {
     return <AppLoading />;
   }
 
   return (
     <View style={styles.cardContainer}>
-      {requests.map((req) => {
-        return (
-          <Pressable
-            key={req.requestUid}
-            onPress={() =>
-              navigation.navigate("RequestScreen", { req, navigation })
-            }
-          >
-            <View style={styles.card}>
-              <Text style={styles.cardTitle}>
-                <Ionicons name={"help-circle"} size={16} color={"#F36433"} />
-                {` Looking for: ${req.title}`}
-              </Text>
-              <Text style={styles.bodyText}>{`"${req.body}"`}</Text>
-              <Text
-                style={styles.cardText}
-              >{`Posted by ${req.userInfo.userUsername}`}</Text>
-            </View>
-          </Pressable>
-        );
-      })}
+      {requests.length
+        ? requests.map((req) => {
+            return (
+              <Pressable
+                key={uuid.v4()}
+                onPress={() =>
+                  navigation.navigate("RequestScreen", { req, navigation })
+                }
+              >
+                <View style={styles.card}>
+                  <Text style={styles.cardTitle}>
+                    <Ionicons
+                      name={"help-circle"}
+                      size={16}
+                      color={"#F36433"}
+                    />
+                    {` Looking for: ${req.title}`}
+                  </Text>
+                  <Text style={styles.bodyText}>{`"${req.body}"`}</Text>
+                  <Text
+                    style={styles.cardText}
+                  >{`Posted by ${req.userInfo.userUsername}`}</Text>
+                </View>
+              </Pressable>
+            );
+          })
+        : null}
     </View>
   );
 };
